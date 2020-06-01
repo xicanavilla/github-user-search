ad152f07de19278230c930d6ede2c2f0169d41ae
Margarita Villa <Mvilla08@gmail.com>
2020-05-31 22:35:17 -0400

    Initial Commit

---
+++ GithubUserSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubUserSearch
+//
+//  Created by Margarita Villa on 5/31/20.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
